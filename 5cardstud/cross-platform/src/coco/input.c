58187b55d515da6558df1f1d019cfb11356bd204
Bill Athing <bill.athing@gmail.com>
2024-08-09 16:03:57 -0700

    Added joystick support

--- 5cardstud/cross-platform/src/coco/input.c
+++ 5cardstud/cross-platform/src/coco/input.c
@@ -1,9 +1,39 @@
 #ifdef _CMOC_VERSION_
 
 //#include <peekpoke.h>
+#include <Joystick.h>
+#include "joystick.h"
 
 unsigned char readJoystick() {
-  return 0; // todo - give real value
+  unsigned char result = 0;
+  unsigned char btns;
+
+  Joystick_selectAnalogMuxInputs(2);  // left joystick horizontal axis
+  if (!Joystick_compareAxis(7))
+  {
+    result |= JOY_LEFT_MASK;
+  }
+  else if (Joystick_compareAxis(56))
+  {
+    result |= JOY_RIGHT_MASK;
+  }  
+
+  Joystick_selectAnalogMuxInputs(3);  // right joystick horizontal axis
+  if (!Joystick_compareAxis(7))
+  {
+    result |= JOY_UP_MASK;
+  }
+  else if (Joystick_compareAxis(56))
+  {
+    result |= JOY_DOWN_MASK;
+  }  
+  btns = Joystick_getButtonStates();
+  if (0==(btns & 0x4)) 
+  {
+    result |= JOY_BTN_1_MASK;
+  }
+
+  return result; 
 }
 
 #endif
